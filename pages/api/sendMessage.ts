9420bf4fb1d7ad70bd8278f47ec05115370091bf
Ashik Chapagain <projectashik@gmail.com>
2021-09-02 00:02:04 +0545

    nextjs-twilio integration completed

---
+++ pages/api/sendMessage.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import twilio from 'twilio';
+
+export default function sendMessage(req: NextApiRequest, res: NextApiResponse) {
+  const accountSid = <string>process.env.TWILIO_ACCOUNT_SID;
+  const token = <string>process.env.TWILIO_AUTH_TOKEN;
+  const client = twilio(accountSid, token);
+  const { phone, message } = req.body;
+  // console.log(phone, message);
+  client.messages
+    .create({
+      body: message,
+      from: '+14784199380',
+      to: phone,
+    })
+    .then((message) =>
+      res.json({
+        success: true,
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        success: false,
+      });
+    });
+}
